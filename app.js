885bde0d348a6258606a67948e7bed9796de7578
Shadow <annduong3721@gmail.com>
2020-09-28 17:36:44 +0000

    finished problem 2

--- app.js
+++ app.js
@@ -7,12 +7,18 @@ function makeNegative(i){
 }
 
 function printAddition(a, b){
-
+  let sum = a + b;
+  console.log(`The sum of ${a} and ${b} is ${sum}.`);
+  
 }
 
 //Write your test cases below this comment
 makeNegative(1) // -1
 makeNegative(5) // -5
+
+printAddition(2, 3)  //the sum of 2 and 3 is 5
+printAddition(9, 8)  //the sum of 9 and 8 is 17
+
 
 //do not editing anything below this comment.
 module.exports = {
